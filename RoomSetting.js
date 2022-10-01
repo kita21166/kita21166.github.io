e3f00d32d720282866681aabb51ffd10b39dbcf1
kita21166 <113743711+kita21166@users.noreply.github.com>
2022-10-02 07:40:01 +0900

    Update RoomSetting.js

--- RoomSetting.js
+++ RoomSetting.js
@@ -1,12 +1,12 @@
 const roomSetting = document.getElementById("RoomSetting"); 
 const setRoomSetting = function() {
-	for (let i = 0; i < 42; i++) {
+	for (let i = 0; i < defaultDeskNumber; i++) {
 		const desk = document.createElement("div");
 		desk.className = "desks";
-		desk.id = `desk_${i}`;
+		desk.dataset.number = `${i}`;
 		desk.dataset.removed = "false";
 		roomSetting.append(desk);
-		desk.addEventListener("click", removeDesk)
+		desk.addEventListener("click", removeDesk);
 	}
 	completeButton.addEventListener("click", checkRemovedDesks);
 }
@@ -21,23 +21,16 @@ const cancelRemoveDesk = function() {
 	this.addEventListener("click", removeDesk);
 }
 const checkRemovedDesks = function() {
-	for (let i = 0; i < 42; i++) {
-		const desk = document.getElementById(`desk_${i}`);
-		if(desk.dataset.removed==="true"){
+	const desk = roomSetting.children;
+	WhereAre = document.getElementById("RoomNameSetting").value;
+	for (let i = 0; i < defaultDeskNumber; i++) {
+		if(desk[i].dataset.removed==="true"){
 			RemovedDesks.push(i);
 		}
 	}
-    howMany = 42 - RemovedDesks.length
-	window.alert(`${howMany}人分の席が登録されました。`);
-	RemoveRoomSetting();
-}
-const RemoveRoomSetting = function() {
+    howMany = defaultDeskNumber - RemovedDesks.length;
+	window.alert(`${WhereAre}の${howMany}人分の席が登録されました。`);
 	document.body.removeChild(document.getElementById("RoomSettingWrap"));
-	document.body.removeChild(completeButton);
-    document.title = "席替え＜R設定＞"
-progress.setTrue(progress.roomNameSetting);
-    const roomNameSetting= document.getElementById("RoomNameSettingWrap");
-    roomNameSetting.className = "blockElement";
-   document.getElementById("submitRName").addEventListener("click",checkRName);    
+    document.title = "席替え＜振り分け＞";
+	SVGmb();
 }
-
