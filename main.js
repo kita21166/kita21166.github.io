df8a9533eed7acc4baafe01eb15ec3b456ff76ce
kita21166 <113743711+kita21166@users.noreply.github.com>
2022-10-04 17:40:18 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,3 @@
 "use strict";
-const RemovedDesks = [],userNames = [],placeName = [],userNums = []
-const completeButton = document.getElementById("CompleteRoomSetting"); 
 const defaultDeskNumber = 42;
-let howMany, howManyRest,randomWhereNum, Animeid, WhereAre;
-    window.addEventListener("load", function() {
-        if (localStorage.getItem("removedDesks") === null) {
-            setRoomSetting();
-        } else {
-            if (window.confirm("以前使用したデータが保存されています。利用しますか？")) {
-                JSON.parse(localStorage.getItem("removedDesks"))
-                    .forEach(ele => {
-                        RemovedDesks.push(ele);
-                    })
-                howMany = JSON.parse(localStorage.getItem("howMany"));
-                WhereAre = JSON.parse(localStorage.getItem("WhereAre"));
-                document.body.removeChild(document.getElementById("RoomSettingWrap"));
-                document.body.removeChild(completeButton);
-                SVGmb();
-            } else {
-                localStorage.clear();
-                setRoomSetting();
-            }
-        }
-    });
+let howManyStudents, RoomName,notAvailableDesks, StudentName;
